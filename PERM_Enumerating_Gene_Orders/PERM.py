dba1e44ab330862af5390dacfacd274d39bbe37e
stefano <s.andreotti7@campus.unimib.it>
2024-04-28 15:29:48 +0200

    Added PERM problem

---
+++ PERM_Enumerating_Gene_Orders/PERM.py
@@ -0,0 +1,27 @@
+"""
+Enumerating Gene Orders
+url: http://rosalind.info/problems/perm/
+
+Given: A positive integer n <= 7.
+Return: The total number of permutations of length n, followed by a list of such permutations (in nay order).
+"""
+import os
+import math
+
+def permute(a, l, r): 
+    if l == r: 
+        print(' '.join(a)) 
+    else: 
+        for i in range(l, r): 
+            a[l], a[i] = a[i], a[l] 
+            permute(a, l+1, r) 
+            a[l], a[i] = a[i], a[l]  # backtrack 
+
+n = int(open(os.getcwd()+'/PERM_Enumerating_Gene_Orders/rosalind_perm.txt', 'r').readline().strip())
+print(math.factorial(n))
+
+perm = []
+[perm.append(str(i+1)) for i in range(n)]
+  
+# Function call 
+permute(perm, 0, n) 
